a7d8c75f5eec78eb752e4c3127d07baaaf6d6761
Mihai Costea <macostea@users.noreply.github.com>
2023-11-28 10:47:52 +0200

    Basic nuxt app structure

---
+++ billboa/pages/clients.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="layout-px-spacing">
+    <div class="middle-content container-xxl p-0">
+      <div class="row" id="cancel-row">
+        <div class="col-xl-12 col-lg-12 col-sm-12 layout-top-spacing layout-spacing">
+          <div class="widget-content widget-content-area br-8">
+            <table class="table dt-table-hover" style="width:100%">
+              <thead>
+                <tr>
+                  <th class="checkbox-column">Record no.</th>
+                  <th>Name</th>
+                  <th>Email</th>
+                  <th>Company ID</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td class="checkbox-column">1</td>
+                  <td>Client 1 SRL</td>
+                  <td>contact@client1.ro</td>
+                  <td>RO12345678</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
