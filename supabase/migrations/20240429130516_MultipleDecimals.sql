854d674c665b826798624563102715794f5fe600
Mihai Costea <macostea@users.noreply.github.com>
2024-04-29 13:19:36 +0000

    Use better precision for prices

---
+++ supabase/migrations/20240429130516_MultipleDecimals.sql
@@ -0,0 +1,4 @@
+alter table invoices alter column total_amount type numeric(20, 4);
+alter table invoices alter column exchange_rate type numeric(20, 4);
+alter table products alter column price type numeric(20, 4);
+alter table products alter column exchange_rate type numeric(20, 4);
